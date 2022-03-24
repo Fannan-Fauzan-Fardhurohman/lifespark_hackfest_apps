757ef645ba506928166944a21b66e8e348d7fd23
Fannan Fauzan Fardhurohman <fannan@jkt1.ebdesk.com>
2022-03-24 23:29:29 +0700

    initial commit

---
+++ app/src/main/java/com/fanxan/lifespark_hackfest/SignInFormActivity.kt
@@ -0,0 +1,11 @@
+package com.fanxan.lifespark_hackfest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_form)
+    }
+}
